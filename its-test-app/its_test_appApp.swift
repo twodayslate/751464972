48e4d388c5bd9f6a1e3c0b2097f440c7a592a921
twodayslate <zac@gorak.us>
2020-11-02 16:17:35 -0500

    Initial Commit

---
+++ its-test-app/its_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  its_test_appApp.swift
+//  its-test-app
+//
+//  Created by Zachary Gorak on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct its_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
